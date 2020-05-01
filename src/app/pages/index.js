815810f50f7557a3c1d240c4159cfa121210fc30
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-01 12:48:35 +0100

    add images to public folder

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,33 +1,79 @@
 import React from "react";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import ProTip from "../components/ProTip";
-import Link from "../components/Link";
-import Copyright from "../components/Copyright";
-import Button from "../components/CustomButtons/Button";
+import classNames from "classnames";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default function Index() {
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Button from "components/CustomButtons/Button.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/landingPage.js";
+
+// Sections for this page
+import ProductSection from "../Sections/ProductSection";
+
+const dashboardRoutes = [];
+
+const useStyles = makeStyles(styles);
+
+export default function Index(props) {
+  const classes = useStyles();
+  const { ...rest } = props;
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        <Button
-          component={Link}
-          color="primary"
-          variant="contained"
-          href="/about"
-          round
-          naked
-        >
-          Go to the about page
-        </Button>
-
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
+    <div>
+      <Header
+        color="transparent"
+        routes={dashboardRoutes}
+        brand="Material Kit React"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 400,
+          color: "white",
+        }}
+        {...rest}
+      />
+      <Parallax filter image="img/landing-bg.jpg">
+        <div className={classes.container}>
+          <GridContainer>
+            <GridItem xs={12} sm={12} md={6}>
+              <h1 className={classes.title}>Your Story Starts With Us.</h1>
+              <h4>
+                Ilorin Emirate Descendants Progressive Union (IEDPU) is the
+                mother of all Unions and Associations in the Ilorin Emirate. It
+                is also the umbrella Union of all indigenes of Ilorin Emirate
+                both at home and abroad. The union has her members and executive
+                members spread all over the five Local Government Areas in the
+                Emirate (Asa, Ilorin East, Ilorin South, Ilorin West and Moro
+                Local Government) Areas and has branches all over the world. The
+                union was formed in 1941 by a group of people before Ilorin
+                Division was dissolved into five differenct Local Government
+                Areas.
+              </h4>
+              <br />
+              <Button
+                color="danger"
+                size="lg"
+                href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&ref=creativetim"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <i className="fas fa-play" />
+                Watch video
+              </Button>
+            </GridItem>
+          </GridContainer>
+        </div>
+      </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div className={classes.container}>
+          <ProductSection />
+        </div>
+      </div>
+      <Footer />
+    </div>
   );
 }
