80e16c2195a7901e8956a132347681a93d745890
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-01 01:41:57 +0100

    fix hooks issue and added support for material-ui

--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,8 +1,24 @@
-import * as React from 'react'
-import App from '../components/App'
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import ProTip from '../components/ProTip';
+import Link from '../components/Link';
+import Copyright from '../components/Copyright';
 
-export default () => (
-  <App>
-    <p>Index Page</p>
-  </App>
-)
+export default function Index() {
+  return (
+    <Container maxWidth="sm">
+      <Box my={4}>
+        <Typography variant="h4" component="h1" gutterBottom>
+          Next.js example
+        </Typography>
+        <Link href="/about" color="secondary">
+          Go to the about page
+        </Link>
+        <ProTip />
+        <Copyright />
+      </Box>
+    </Container>
+  );
+}
