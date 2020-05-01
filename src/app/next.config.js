db567d6bc12355ebf1db166448f68f8db6af262e
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-01 12:49:36 +0100

    add components path to webpack config

--- src/app/next.config.js
+++ src/app/next.config.js
@@ -4,6 +4,7 @@ module.exports = {
   distDir: "../../dist/functions/next",
   webpack: (config, options) => {
     config.resolve.alias["assets"] = path.resolve(__dirname, "assets/");
+    config.resolve.alias["components"] = path.resolve(__dirname, "components/");
     return config;
   },
 };
