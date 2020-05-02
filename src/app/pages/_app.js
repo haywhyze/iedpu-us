6382f64d503b3443095ed0a0f111cae42484d521
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-02 15:19:46 +0100

    add suport for sass/scss and update page title

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -5,6 +5,8 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../components/theme";
 
+import "assets/scss/material-kit-react.scss?v=1.8.0";
+
 export default function MyApp(props) {
   const { Component, pageProps } = props;
 
@@ -19,7 +21,7 @@ export default function MyApp(props) {
   return (
     <React.Fragment>
       <Head>
-        <title>My page</title>
+        <title>IEDPU - US</title>
         <meta
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
