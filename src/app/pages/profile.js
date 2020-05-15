ceb0a70730e3a80e6731483d27950291c4009d7c
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-15 11:16:06 +0100

    add profile page for logged in user

---
+++ src/app/pages/profile.js
@@ -0,0 +1,79 @@
+import React, { useContext } from "react";
+import { useRouter } from "next/router";
+// nodejs library that concatenates classes
+import classNames from "classnames";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import Header from "components/Header/Header.js";
+import Footer from "components/Footer/Footer.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import HeaderLinks from "components/Header/HeaderLinks.js";
+import Parallax from "components/Parallax/Parallax.js";
+
+import styles from "assets/jss/material-kit-react/views/profilePage.js";
+import { AuthContext } from "./_app";
+
+const useStyles = makeStyles(styles);
+
+function ProfilePage(props) {
+  const classes = useStyles();
+  const router = useRouter();
+  const { ...rest } = props;
+  const imageClasses = classNames(
+    classes.imgRaised,
+    classes.imgRoundedCircle,
+    classes.imgFluid
+  );
+
+  const { user, isAuthenticated } = useContext(AuthContext);
+
+  if (!isAuthenticated) {
+      router.push("/login")
+  }
+
+  return (
+    <div>
+      <Header
+        color="transparent"
+        brand="IEDPU"
+        rightLinks={<HeaderLinks />}
+        fixed
+        changeColorOnScroll={{
+          height: 200,
+          color: "white",
+        }}
+        {...rest}
+      />
+      <Parallax small filter image="img/central-mosque-1.jpg" />
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GridContainer justify="center">
+              <GridItem xs={12} sm={12} md={6}>
+                <div className={classes.profile}>
+                  <div>
+                    <img
+                      src={user && user.photoURL}
+                      alt="..."
+                      className={imageClasses}
+                    />
+                  </div>
+                  <div className={classes.name}>
+                    <h3 className={classes.title}>
+                      {user && user.displayName}
+                    </h3>
+                  </div>
+                </div>
+              </GridItem>
+            </GridContainer>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default ProfilePage;
