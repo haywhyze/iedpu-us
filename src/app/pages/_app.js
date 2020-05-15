e62c7dc789cf8f69167d6995f01dfb31eaa5a048
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-15 11:16:46 +0100

    initialise firbase auth and made available to other components through context

--- src/app/pages/_app.js
+++ src/app/pages/_app.js
@@ -5,10 +5,46 @@ import { ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import theme from "../components/theme";
 
+import firebaseConfig from "../../firebaseConfig";
+import withFirebaseAuth from "react-with-firebase-auth";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+
+const firebaseApp = !firebase.apps.length
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+const firebaseAppAuth = firebaseApp.auth();
+
+let isAuthenticated = false;
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (user) {
+    // User is signed in.
+    isAuthenticated = true;
+  } else {
+    // No user is signed in.
+    isAuthenticated = false;
+  }
+});
+
+const providers = {
+  facebookProvider: new firebase.auth.FacebookAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
 import "assets/scss/material-kit-react.scss?v=1.8.0";
 
-export default function MyApp(props) {
+export const AuthContext = React.createContext();
+function MyApp(props) {
   const { Component, pageProps } = props;
+  const {
+    user,
+    signOut,
+    signInWithFacebook,
+    signInWithGoogle,
+    loading,
+  } = props;
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
@@ -28,13 +64,29 @@ export default function MyApp(props) {
         />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+        <AuthContext.Provider
+          value={{
+            user,
+            signOut,
+            signInWithFacebook,
+            signInWithGoogle,
+            loading,
+            isAuthenticated,
+          }}
+        >
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </AuthContext.Provider>
       </ThemeProvider>
     </React.Fragment>
   );
 }
+
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(MyApp);
 
 MyApp.propTypes = {
   Component: PropTypes.elementType.isRequired,
