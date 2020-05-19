b7e4a6fc28b7ca3748b75ea2bea29e56f5d92b66
haywhyze <haywhyze@gmail.com>
2020-05-19 08:11:18 +0100

    add more fields to profile update

--- src/app/Sections/ProfileUpdate.js
+++ src/app/Sections/ProfileUpdate.js
@@ -5,7 +5,11 @@ import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
 // @material-ui/icons
 import People from "@material-ui/icons/People";
+import Phone from "@material-ui/icons/Phone";
 import Email from "@material-ui/icons/Email";
+import Work from "@material-ui/icons/Work";
+import Info from "@material-ui/icons/Info";
+import LocationCity from "@material-ui/icons/LocationCity";
 // core components
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -18,56 +22,64 @@ import { db } from "../pages/_app";
 
 const useStyles = makeStyles(styles);
 
-const docRef = db.collection("Users").doc("SF");
-
-docRef
-  .get()
-  .then(function (doc) {
-    if (doc.exists) {
-      console.log("Document data:", doc.data());
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-    }
-  })
-  .catch(function (error) {
-    console.log("Error getting document:", error);
-  });
-
-export default function SectionLogin() {
+export default function ProfileUpdate() {
   const classes = useStyles();
   const { user } = useContext(AuthContext);
-  const [displayName, setDisplayName] = useState("");
+  const [values, setValues] = useState({
+    displayName: "",
+    email: "",
+    bio: "",
+    phone: "",
+    location: "",
+    occupation: "",
+  });
 
   const docRef = user && db.collection("Users").doc(user.uid);
 
   React.useEffect(() => {
-    docRef
-      .get()
-      .then(function (doc) {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          setDisplayName(doc.data().displayName);
-        } else {
-          // doc.data() will be undefined in this case
-          console.log("No such document!");
-        }
-      })
-      .catch(function (error) {
-        console.log("Error getting document:", error);
-      });
+    if (user)
+      docRef
+        .get()
+        .then(function (doc) {
+          if (doc.exists) {
+            const {
+              displayName,
+              email,
+              bio,
+              occupation,
+              phone,
+              location,
+            } = doc.data();
+            setValues({
+              ...values,
+              displayName,
+              email,
+              bio,
+              occupation,
+              phone,
+              location,
+            });
+          } else {
+            // doc.data() will be undefined in this case
+            console.log("No such document!");
+          }
+        })
+        .catch(function (error) {
+          console.log("Error getting document:", error);
+        });
   }, []);
 
-  const handleChange = (e) => {
-    setDisplayName(e.target.value);
+  const _handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
   };
 
   const updateProfile = () => {
     db.collection("Users")
       .doc(user.uid)
-      .set({
-        displayName,
-      })
+      .set(values)
       .then(function () {
         console.log("Document successfully written!");
       })
@@ -89,17 +101,109 @@ export default function SectionLogin() {
             <form className={classes.form}>
               <CustomInput
                 labelText="Full Name"
-                id="first"
+                id="name"
                 formControlProps={{
                   fullWidth: true,
                 }}
                 inputProps={{
                   type: "text",
-                  value: displayName,
-                  onChange: handleChange,
+                  name: "displayName",
+                  value: values.displayName,
+                  onChange: _handleChange,
                   endAdornment: (
                     <InputAdornment position="end">
                       <People className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <CustomInput
+                labelText="Email"
+                id="email"
+                formControlProps={{
+                  fullWidth: true,
+                }}
+                inputProps={{
+                  type: "text",
+                  name: "email",
+                  disabled: true,
+                  value: values.email,
+                  onChange: _handleChange,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Email className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <CustomInput
+                labelText="Phone Number"
+                id="phone_number"
+                formControlProps={{
+                  fullWidth: true,
+                }}
+                inputProps={{
+                  type: "text",
+                  name: "phone",
+                  value: values.phone,
+                  onChange: _handleChange,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Phone className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <CustomInput
+                labelText="Bio"
+                id="bio"
+                formControlProps={{
+                  fullWidth: true,
+                }}
+                inputProps={{
+                  name: "bio",
+                  type: "text",
+                  value: values.bio,
+                  onChange: _handleChange,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Info className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <CustomInput
+                labelText="Location"
+                id="location"
+                formControlProps={{
+                  fullWidth: true,
+                }}
+                inputProps={{
+                  name: "location",
+                  type: "text",
+                  value: values.location,
+                  onChange: _handleChange,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <LocationCity className={classes.inputIconsColor} />
+                    </InputAdornment>
+                  ),
+                }}
+              />
+              <CustomInput
+                labelText="Occupation"
+                id="occupation"
+                formControlProps={{
+                  fullWidth: true,
+                }}
+                inputProps={{
+                  name: "occupation",
+                  type: "text",
+                  value: values.occupation,
+                  onChange: _handleChange,
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <Work className={classes.inputIconsColor} />
                     </InputAdornment>
                   ),
                 }}
