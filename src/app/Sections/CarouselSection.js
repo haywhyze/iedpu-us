dc4819a4247dfc2ebf2d698f5f135f9cfffdd46e
haywhyze <haywhyze@gmail.com>
2020-05-20 18:06:14 +0100

    change objectFit to cover

--- src/app/Sections/CarouselSection.js
+++ src/app/Sections/CarouselSection.js
@@ -36,7 +36,7 @@ export default function SectionCarousel() {
                     src="img/bg.jpg"
                     alt="First slide"
                     className="slick-image"
-                    style={{ height: "25rem", objectFit: "contain" }}
+                    style={{ height: "25rem", objectFit: "cover" }}
                   />
                 </div>
                 <div>
@@ -44,7 +44,7 @@ export default function SectionCarousel() {
                     src="img/bg2.jpg"
                     alt="Second slide"
                     className="slick-image"
-                    style={{ height: "25rem", objectFit: "contain" }}
+                    style={{ height: "25rem", objectFit: "cover" }}
                   />
                 </div>
                 <div>
@@ -52,7 +52,7 @@ export default function SectionCarousel() {
                     src="img/bg3.jpg"
                     alt="Third slide"
                     className="slick-image"
-                    style={{ height: "25rem", objectFit: "contain" }}
+                    style={{ height: "25rem", objectFit: "cover" }}
                   />
                 </div>
                 <div>
@@ -60,15 +60,15 @@ export default function SectionCarousel() {
                     src="img/bg4.jpg"
                     alt="Third slide"
                     className="slick-image"
-                    style={{ height: "25rem", objectFit: "contain" }}
+                    style={{ height: "25rem", objectFit: "cover" }}
                   />
                 </div>
                 <div>
                   <img
-                    src="img/bg7.jpg"
+                    src="img/bg7.png"
                     alt="Third slide"
                     className="slick-image"
-                    style={{ height: "25rem", objectFit: "contain" }}
+                    style={{ height: "25rem", objectFit: "cover" }}
                   />
                 </div>
               </Carousel>
