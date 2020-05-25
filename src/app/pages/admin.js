aa934816528b0555c2c9bfaf505d93edd6b2eba5
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-05-25 19:28:03 +0100

    wip

---
+++ src/app/pages/admin.js
@@ -0,0 +1,83 @@
+import React from "react";
+import {
+  Switch,
+  Route,
+  Redirect,
+  BrowserRouter as Router,
+} from "react-router-dom";
+// import Router from "next/router";
+// @material-ui/core components
+import { makeStyles } from "@material-ui/core/styles";
+// core components
+import Navbar from "components/Navbars/Navbar.js";
+import Footer from "components/Footer/Footer.js";
+import Sidebar from "components/Sidebar/Sidebar.js";
+
+import routes from "../routes";
+
+import styles from "assets/jss/material-dashboard-react/layouts/adminStyle.js";
+
+import bgImage from "assets/img/sidebar-2.jpg";
+import logo from "assets/img/reactlogo.png";
+
+const switchRoutes = (
+  <Router>
+    <Switch>
+      {routes.map((prop, key) => {
+        if (prop.layout === "/admin") {
+          return (
+            <Route
+              path={prop.layout + prop.path}
+              component={prop.component}
+              key={key}
+            />
+          );
+        }
+        return null;
+      })}
+      <Redirect from="/admin" to="/admin/dashboard" />
+    </Switch>
+  </Router>
+);
+
+const useStyles = makeStyles(styles);
+
+export default function Admin({ ...rest }) {
+  // styles
+  const classes = useStyles();
+  // ref to help us initialize PerfectScrollbar on windows devices
+  const mainPanel = React.createRef();
+  // states and functions
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
+  return (
+    <div className={classes.wrapper}>
+      <Sidebar
+        routes={routes}
+        logoText={"Creative Tim"}
+        logo={logo}
+        image={bgImage}
+        handleDrawerToggle={handleDrawerToggle}
+        open={mobileOpen}
+        color={"purple"}
+        {...rest}
+      />
+      <div className={classes.mainPanel} ref={mainPanel}>
+        <Navbar
+          routes={routes}
+          handleDrawerToggle={handleDrawerToggle}
+          {...rest}
+        />
+        {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
+        <div className={classes.content}>
+          <div className={classes.container}>{switchRoutes}</div>
+        </div>
+        <Footer />
+      </div>
+    </div>
+  );
+}
