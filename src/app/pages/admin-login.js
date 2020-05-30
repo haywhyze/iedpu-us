a6708ba83e5a678894ff5025cd9b316b05ce7ddb
haywhyze <haywhyze@gmail.com>
2020-05-30 08:45:33 +0100

    implement admin login

--- src/app/pages/admin-login.js
+++ src/app/pages/admin-login.js
@@ -1,15 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import Router from "next/router";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import InputAdornment from "@material-ui/core/InputAdornment";
 import Icon from "@material-ui/core/Icon";
-
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Router from "next/router";
 import Email from "@material-ui/icons/Email";
-import People from "@material-ui/icons/People";
-import Link from "next/link";
 import Footer from "components/Footer/Footer.js";
 import GridContainer from "components/Grid/GridContainer.js";
 import GridItem from "components/Grid/GridItem.js";
@@ -18,6 +13,7 @@ import Card from "components/Card/Card.js";
 import CardBody from "components/Card/CardBody.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CustomInput from "components/CustomInput/CustomInput.js";
+import SnackbarContent from "components/Snackbar/SnackbarContent.js";
 import CardFooter from "components/Card/CardFooter.js";
 
 import styles from "assets/jss/material-kit-react/views/loginPage.js";
@@ -31,18 +27,33 @@ function LoginPage(props) {
     setCardAnimation("");
   }, 700);
   const classes = useStyles();
-  const { signInWithFacebook, signInWithGoogle, isAuthenticated } = useContext(
-    AuthContext
-  );
+  const {
+    error,
+    isAuthenticated,
+    signInWithEmailAndPassword,
+    setError,
+    loading,
+  } = useContext(AuthContext);
 
   const { ...rest } = props;
 
   React.useEffect(() => {
     if (isAuthenticated) {
-      // Router.push("/admin/dashboard");
-      console.log("object");
+      Router.push("/admin/dashboard");
     }
   }, [isAuthenticated]);
+
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
+    });
+  };
 
   return (
     <div>
@@ -63,46 +74,80 @@ function LoginPage(props) {
                     <h3>Login as Admin</h3>
                   </CardHeader>
                   <CardBody>
-                    <CustomInput
-                      labelText="Email..."
-                      id="email"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        type: "email",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Email className={classes.inputIconsColor} />
-                          </InputAdornment>
-                        ),
-                      }}
-                    />
-                    <CustomInput
-                      labelText="Password"
-                      id="pass"
-                      formControlProps={{
-                        fullWidth: true,
-                      }}
-                      inputProps={{
-                        type: "password",
-                        endAdornment: (
-                          <InputAdornment position="end">
-                            <Icon className={classes.inputIconsColor}>
-                              lock_outline
-                            </Icon>
-                          </InputAdornment>
-                        ),
-                        autoComplete: "off",
-                      }}
-                    />
+                    {loading && <p>logging in...</p>}
+                    {error && (
+                      <SnackbarContent
+                        message={<span>ERROR: {error}</span>}
+                        color="danger"
+                      />
+                    )}
+                    {!isAuthenticated && (
+                      <CustomInput
+                        labelText="Email..."
+                        id="email"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                        inputProps={{
+                          type: "email",
+                          name: "email",
+                          value: values.email,
+                          onChange: handleChange,
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Email className={classes.inputIconsColor} />
+                            </InputAdornment>
+                          ),
+                        }}
+                      />
+                    )}
+                    {!isAuthenticated && (
+                      <CustomInput
+                        labelText="Password"
+                        id="pass"
+                        formControlProps={{
+                          fullWidth: true,
+                        }}
+                        inputProps={{
+                          type: "password",
+                          name: "password",
+                          value: values.password,
+                          onChange: handleChange,
+                          endAdornment: (
+                            <InputAdornment position="end">
+                              <Icon className={classes.inputIconsColor}>
+                                lock_outline
+                              </Icon>
+                            </InputAdornment>
+                          ),
+                          autoComplete: "off",
+                        }}
+                      />
+                    )}
                   </CardBody>
                   <CardFooter className={classes.cardFooter}>
-                    <Link href="/admin">
-                      <Button simple color="primary" size="lg">
+                    {isAuthenticated && Router.push("/admin/dashboard")}
+                    {!isAuthenticated && (
+                      <Button
+                        onClick={() =>
+                          signInWithEmailAndPassword(
+                            values.email,
+                            values.password
+                          )
+                        }
+                        color="primary"
+                      >
                         LOG IN
                       </Button>
-                    </Link>
+                    )}
+                    {error && (
+                      <Button
+                        color="transparent"
+                        onClick={() => setError(null)}
+                      >
+                        Clear Error
+                      </Button>
+                    )}
                   </CardFooter>
                 </form>
               </Card>
