4530d12a45b6f0412b300a67fa61e8a68814c3d7
haywhyze <haywhyze@gmail.com>
2020-06-14 13:30:36 +0100

    format profile.js

--- src/app/pages/profile.js
+++ src/app/pages/profile.js
@@ -1,27 +1,30 @@
-import React, { useContext, useState } from "react";
-import Router from "next/router";
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
+import React, { useContext, useState } from 'react';
+import Router from 'next/router';
 // nodejs library that concatenates classes
-import classNames from "classnames";
+import classNames from 'classnames';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 // core components
-import { css } from "@emotion/core";
+// import { css } from '@emotion/core';
 
-import Header from "components/Header/Header.js";
-import Footer from "components/Footer/Footer.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
-import SectionTabs from "../Sections/SectionTabs";
-import AddAPhoto from "@material-ui/icons/AddAPhoto";
-import Button from "components/CustomButtons/Button.js";
-import CircularProgress from "@material-ui/core/CircularProgress";
-import { db } from "./_app";
+import Header from 'components/Header/Header.js';
+import Footer from 'components/Footer/Footer.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import HeaderLinks from 'components/Header/HeaderLinks.js';
+import Parallax from 'components/Parallax/Parallax.js';
+import AddAPhoto from '@material-ui/icons/AddAPhoto';
+import Button from 'components/CustomButtons/Button.js';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import styles from 'assets/jss/material-kit-react/views/profilePage.js';
+import { db, AuthContext } from './_app';
 
-import styles from "assets/jss/material-kit-react/views/profilePage.js";
-import { AuthContext } from "./_app";
-import UnderReview from "../Sections/UnderReview";
+import SectionTabs from '../Sections/SectionTabs';
+
+import UnderReview from '../Sections/UnderReview';
 
 const useStyles = makeStyles(styles);
 
@@ -31,76 +34,79 @@ function ProfilePage(props) {
   const imageClasses = classNames(
     classes.imgRaised,
     classes.imgRoundedCircle,
-    classes.imgFluid
+    classes.imgFluid,
   );
-  const { user, isAuthenticated, isAdmin, authLoading } = useContext(
-    AuthContext
+  const {
+    user, isAuthenticated, isAdmin, authLoading,
+  } = useContext(
+    AuthContext,
   );
   const [displayName, setDisplayName] = useState(
-    (user && user.displayName) || ""
+    (user && user.displayName) || '',
   );
-  const [photoURL, setPhotoURL] = useState((user && user.photoURL) || "");
+  const [photoURL, setPhotoURL] = useState((user && user.photoURL) || '');
   const [verified, setVerified] = useState(false);
   const [loading, setLoading] = useState(true);
 
-  const override = css`
-    display: block;
-    margin: 0 auto;
-    border-color: purple;
-  `;
+  // const override = css`
+  //   display: block;
+  //   margin: 0 auto;
+  //   border-color: purple;
+  // `;
 
-  !isAdmin &&
-    user &&
+  if (!isAdmin
+    && user) {
     db
-      .collection("Users")
+      .collection('Users')
       .doc(user.uid)
-      .onSnapshot(function (doc) {
+      .onSnapshot((doc) => {
         if (doc.exists) {
           setLoading(false);
           if (!doc.data().verified) {
             setVerified(false);
             return;
-          } else setVerified(true);
+          } setVerified(true);
           setDisplayName(doc.data().displayName);
           setPhotoURL(doc.data().photoURL);
         } else setLoading(false);
       });
+  }
 
   React.useEffect(() => {
     if (!isAuthenticated) {
-      Router.push("/login");
+      Router.push('/login');
     }
   }, [isAuthenticated]);
 
   React.useEffect(() => {
-    Router.prefetch("/login");
+    Router.prefetch('/login');
   }, []);
 
   const changePhoto = () => {
     window.cloudinary.openUploadWidget(
       {
-        cloudName: "haywhyze",
-        uploadPreset: "ittv3vvm",
-        sources: ["local", "camera", "facebook", "instagram"],
+        cloudName: 'haywhyze',
+        uploadPreset: 'ittv3vvm',
+        sources: ['local', 'camera', 'facebook', 'instagram'],
         showAdvancedOptions: true,
         cropping: true,
         multiple: false,
-        defaultSource: "local",
+        defaultSource: 'local',
         styles: {
           palette: {
-            window: "#FFFFFF",
-            windowBorder: "#90A0B3",
-            tabIcon: "#0078FF",
-            menuIcons: "#5A616A",
-            textDark: "#000000",
-            textLight: "#FFFFFF",
-            link: "#0078FF",
-            action: "#FF620C",
-            inactiveTabIcon: "#0E2F5A",
-            error: "#F44235",
-            inProgress: "#0078FF",
-            complete: "#20B832",
-            sourceBg: "#E4EBF1",
+            window: '#FFFFFF',
+            windowBorder: '#90A0B3',
+            tabIcon: '#0078FF',
+            menuIcons: '#5A616A',
+            textDark: '#000000',
+            textLight: '#FFFFFF',
+            link: '#0078FF',
+            action: '#FF620C',
+            inactiveTabIcon: '#0E2F5A',
+            error: '#F44235',
+            inProgress: '#0078FF',
+            complete: '#20B832',
+            sourceBg: '#E4EBF1',
           },
           fonts: {
             default: {
@@ -111,37 +117,38 @@ function ProfilePage(props) {
       },
       (err, info) => {
         if (!err) {
-          if (info.event === "success") {
+          if (info.event === 'success') {
             // console.log(info.info.secure_url)
-            db.collection("Users")
+            db.collection('Users')
               .doc(user.uid)
               .update({ photoURL: info.info.secure_url })
-              .then(function () {
-                console.log("Document successfully written!");
+              .then(() => {
+                console.log('Document successfully written!');
               })
-              .catch(function (error) {
-                console.error("Error writing document: ", error);
+              .catch((error) => {
+                console.error('Error writing document: ', error);
               });
-            console.log("Upload Widget event - ", info);
+            console.log('Upload Widget event - ', info);
           }
         }
-      }
+      },
     );
   };
 
-  if (authLoading || loading)
+  if (authLoading || loading) {
     return (
       <div
         style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          minHeight: "100vh",
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '100vh',
         }}
       >
         <CircularProgress />
       </div>
     );
+  }
 
   if (!isAdmin && !verified) {
     return <UnderReview />;
@@ -156,7 +163,7 @@ function ProfilePage(props) {
         fixed
         changeColorOnScroll={{
           height: 200,
-          color: "white",
+          color: 'white',
         }}
         {...rest}
       />
@@ -169,7 +176,7 @@ function ProfilePage(props) {
                 <div className={classes.profile}>
                   <div>
                     <img
-                      src={photoURL + "?height=400" || "img/profile.png"}
+                      src={`${photoURL}?height=400` || 'img/profile.png'}
                       alt="..."
                       className={imageClasses}
                     />
@@ -177,9 +184,9 @@ function ProfilePage(props) {
                       onClick={changePhoto}
                       color="transparent"
                       style={{
-                        padding: "0.2rem 0.9375rem",
-                        fontWeight: "400",
-                        fontSize: "12px",
+                        padding: '0.2rem 0.9375rem',
+                        fontWeight: '400',
+                        fontSize: '12px',
                       }}
                     >
                       <AddAPhoto fontSize="small" />
