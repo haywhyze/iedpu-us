442f31c362e8cad79996f9a37dd59c1920debb7e
haywhyze <haywhyze@gmail.com>
2020-06-21 17:22:23 +0100

    implement image gallery

--- src/app/pages/gallery.js
+++ src/app/pages/gallery.js
@@ -1,15 +1,18 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import classNames from 'classnames';
 
-import Header from "components/Header/Header.js";
-import HeaderLinks from "components/Header/HeaderLinks.js";
-import Parallax from "components/Parallax/Parallax.js";
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import Small from "components/Typography/Small.js";
+import Header from 'components/Header/Header.js';
+import HeaderLinks from 'components/Header/HeaderLinks.js';
+import Parallax from 'components/Parallax/Parallax.js';
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import Small from 'components/Typography/Small.js';
+import Footer from 'components/Footer/Footer.js';
 
-import styles from "assets/jss/material-kit-react/views/profilePage.js";
-import typoStyles from "assets/jss/material-kit-react/views/componentsSections/typographyStyle.js";
+import styles from 'assets/jss/material-kit-react/views/profilePage.js';
+import typoStyles from 'assets/jss/material-kit-react/views/componentsSections/typographyStyle.js';
+import GalleryContainer from '../Sections/Gallery/GalleryContainer';
 
 const useStyles = makeStyles(styles);
 const useTypoStyles = makeStyles(typoStyles);
@@ -27,8 +30,8 @@ export default function Gallery(props) {
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
-          height: 300,
-          color: "white",
+          height: 50,
+          color: 'white',
         }}
         {...rest}
       />
@@ -37,21 +40,29 @@ export default function Gallery(props) {
           <GridContainer justify="center">
             <GridItem xs={12} sm={12} md={6}>
               <div className={typoClasses.typo}>
-                <h1 className={classes.title} style={{ color: "#fff" }}>
+                <h1 className={classes.title} style={{ color: '#fff' }}>
                   Gallery
                 </h1>
                 <h2>
                   <Small>
-                    <span style={{ color: "#fff" }}>
+                    <span style={{ color: '#fff' }}>
                       Showcasing lovely photos of Ilorin, its people, culture and heritage
                     </span>
                   </Small>
                 </h2>
               </div>
-            </GridItem>
+            </GridItem> 
           </GridContainer>
         </div>
       </Parallax>
+      <div className={classNames(classes.main, classes.mainRaised)}>
+        <div>
+          <div className={classes.container}>
+            <GalleryContainer />
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
