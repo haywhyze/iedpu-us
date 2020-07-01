95e013ca12b58e978356ea14f1c520a9542b4707
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-07-01 12:31:04 +0100

    format membersList

--- src/app/Sections/Profile/MembersList.js
+++ src/app/Sections/Profile/MembersList.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect, useContext } from "react";
-import { AuthContext } from "../../pages/_app";
-import { db } from "../../pages/_app";
-import ProfileModal from "./ProfileModal";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import Divider from "@material-ui/core/Divider";
-import ListItemText from "@material-ui/core/ListItemText";
-import ListItemAvatar from "@material-ui/core/ListItemAvatar";
-import Avatar from "@material-ui/core/Avatar";
-import LaunchRoundedIcon from "@material-ui/icons/LaunchRounded";
-import Button from "components/CustomButtons/Button.js";
+import React, { useState, useEffect, useContext } from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import Divider from '@material-ui/core/Divider';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
+import LaunchRoundedIcon from '@material-ui/icons/LaunchRounded';
+import Button from 'components/CustomButtons/Button';
+import ProfileModal from './ProfileModal';
+import { db, AuthContext } from '../../pages/_app';
 
 export default function MembersList() {
   const { user } = useContext(AuthContext);
 
-  const docRef = user && db.collection("Users").where("verified", "==", true).orderBy("displayName");
+  const docRef = user
+    && db.collection('Users').where('verified', '==', true).orderBy('displayName');
 
   const [classicModal, setClassicModal] = React.useState(false);
 
@@ -28,20 +28,21 @@ export default function MembersList() {
   };
 
   useEffect(() => {
-    if (user)
+    if (user) {
       docRef
         .get()
         .then((querySnapshot) => {
           const data = [];
-          querySnapshot.forEach(function (doc) {
+          querySnapshot.forEach((doc) => {
             // doc.data() is never undefined for query doc snapshots
             data.push({ ...doc.data(), id: doc.id });
           });
           setMembers(data);
         })
-        .catch(function (error) {
-          console.log("Error getting document:", error);
+        .catch((error) => {
+          console.log('Error getting document:', error);
         });
+    }
   }, []);
 
   return (
@@ -52,39 +53,37 @@ export default function MembersList() {
         member={selectedMember}
       />
       <List>
-        {members.map((member, index) => {
-          return (
-            <div key={member.id}>
-              <ListItem alignItems="flex-start">
-                <ListItemAvatar>
-                  <Avatar alt={member.displayName} src={member.photoURL} />
-                </ListItemAvatar>
-                <ListItemText
-                  primary={
-                    <React.Fragment>
-                      {member.displayName}
-                      <Button
-                        onClick={() => viewMember(member)}
-                        color="transparent"
-                        style={{
-                          padding: "0.2rem 0.9375rem",
-                          fontWeight: "400",
-                          fontSize: "12px",
-                        }}
-                      >
-                        <LaunchRoundedIcon fontSize="small" />
-                      </Button>
-                    </React.Fragment>
-                  }
-                  secondary={member.location || ""}
-                />
-              </ListItem>
-              {members.length - 1 !== index && (
-                <Divider variant="inset" component="li" />
-              )}
-            </div>
-          );
-        })}
+        {members.map((member, index) => (
+          <div key={member.id}>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar alt={member.displayName} src={member.photoURL} />
+              </ListItemAvatar>
+              <ListItemText
+                primary={(
+                  <>
+                    {member.displayName}
+                    <Button
+                      onClick={() => viewMember(member)}
+                      color="transparent"
+                      style={{
+                        padding: '0.2rem 0.9375rem',
+                        fontWeight: '400',
+                        fontSize: '12px',
+                      }}
+                    >
+                      <LaunchRoundedIcon fontSize="small" />
+                    </Button>
+                  </>
+                )}
+                secondary={member.location || ''}
+              />
+            </ListItem>
+            {members.length - 1 !== index && (
+              <Divider variant="inset" component="li" />
+            )}
+          </div>
+        ))}
       </List>
     </>
   );
