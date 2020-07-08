92b465162bfd7b33c860c3d1f0049689a25b2bb7
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-07-08 17:19:58 +0100

    update about page with stakeholders biodata

--- src/app/pages/about.js
+++ src/app/pages/about.js
@@ -7,7 +7,9 @@ import HeaderLinks from 'components/Header/HeaderLinks.js';
 import Parallax from 'components/Parallax/Parallax.js';
 import GridContainer from 'components/Grid/GridContainer.js';
 import GridItem from 'components/Grid/GridItem.js';
+import Button from 'components/CustomButtons/Button.js';
 import Small from 'components/Typography/Small.js';
+import Link from 'next/link';
 import CustomTabs from 'components/CustomTabs/CustomTabs.js';
 import Footer from 'components/Footer/Footer.js';
 
@@ -16,6 +18,7 @@ import styles2 from 'assets/jss/material-kit-react/views/landingPage.js';
 import typoStyles from 'assets/jss/material-kit-react/views/componentsSections/typographyStyle.js';
 import teamStyles from 'assets/jss/material-kit-react/views/landingPageSections/teamStyle.js';
 
+import { Typography } from '@material-ui/core';
 import organizationChartData from '../Sections/OrganizationCharts/data';
 import Panel from '../Sections/OrganizationCharts/Panel';
 
@@ -30,13 +33,13 @@ export default function Executives(props) {
   const typoClasses = useTypoStyles();
   const teamClasses = useTeamStyles();
   const { ...rest } = props;
-  const { executives } = organizationChartData;
+  const { executives, bot, ac } = organizationChartData;
   return (
     <div>
       <Header
         color="transparent"
         routes={[]}
-        brand="IEDPU - USA"
+        brand={<img style={{ width: '4rem', margin: '-2rem' }} src="img/logo.png" />}
         rightLinks={<HeaderLinks />}
         fixed
         changeColorOnScroll={{
@@ -57,7 +60,8 @@ export default function Executives(props) {
                 <h2>
                   <Small>
                     <span style={{ color: '#fff' }}>
-                      Get to know about Ilorin Emirate Descendants Progressive Union in the USA and the amazing people behind it.
+                      Get to know about Ilorin Emirate Descendants Progressive
+                      Union in the USA and the amazing people behind it.
                     </span>
                   </Small>
                 </h2>
@@ -81,9 +85,92 @@ export default function Executives(props) {
                           {
                             tabName: 'About',
                             tabContent: (
-                              <>
-                                <h3>About IEDPU - USA</h3>
-                              </>
+                              <div
+                                style={{
+                                  textAlign: 'center',
+                                  margin: '2rem 10rem',
+                                  lineHeight: 1.5,
+                                  fontSize: '150%',
+                                }}
+                              >
+                                <h3
+                                  style={{ fontSize: '150%', lineHeight: 1.5 }}
+                                >
+                                  About IEDPU - USA
+                                </h3>
+                                <Typography
+                                  variant="body1"
+                                  style={{
+                                    textAlign: 'justify',
+                                    fontSize: '100%',
+                                    lineHeight: 1.5,
+                                  }}
+                                  gutterBottom
+                                >
+                                  We, the indigenes of ILORIN EMIRATE from Kwara
+                                  State of Nigeria residing in United States
+                                  (North-America) have come together to find
+                                  ways and means of improving ourselves in
+                                  DIASPORA, and contributing to the development
+                                  of our dearly beloved Emirate, thus providing
+                                  economic activities that will have greater
+                                  impact on our indigenes both in the United
+                                  States and at home.
+                                </Typography>
+                                <br />
+                                <Typography
+                                  variant="body1"
+                                  style={{
+                                    textAlign: 'justify',
+                                    fontSize: '100%',
+                                    lineHeight: 1.5,
+                                  }}
+                                  gutterBottom
+                                >
+                                  ILORIN EMIRATE DESCENDANTS PROGRESSIVE UNION,
+                                  United States (North-America) is a non-profit
+                                  organization organized for educational, Social
+                                  and charitable purposes within the meaning of
+                                  Section 501(c) 3 of the Internal Revenue Code
+                                  of the United States of America.
+                                </Typography>
+                                <br />
+                                <Typography
+                                  variant="body1"
+                                  style={{
+                                    textAlign: 'justify',
+                                    fontSize: '100%',
+                                    lineHeight: 1.5,
+                                  }}
+                                  gutterBottom
+                                >
+                                  The union was founded to promote and foster
+                                  unity amongst Ilorin Emirate indigenes in
+                                  United States (North-America). IEDPU-USA is to
+                                  promote the cultural heritage of Ilorin
+                                  Emirate, foster economic growth, initiate and
+                                  encourage programs/projects that will have
+                                  impact on the lives of our kinsmen living in
+                                  the United States and at home.
+                                </Typography>
+                                <br />
+                                <div>
+                                  <a href="/iedpu-us_constitution.pdf" download>
+                                    <span>
+                                      <Button color="primary" size="lg" round>
+                                        Download Constitution
+                                      </Button>
+                                    </span>
+                                  </a>
+                                  <a href="/iedpu-us_constitution.pdf">
+                                    <span>
+                                      <Button color="transparent" size="lg" round>
+                                        Read Constitution
+                                      </Button>
+                                    </span>
+                                  </a>
+                                </div>
+                              </div>
                             ),
                           },
                           {
@@ -98,7 +185,7 @@ export default function Executives(props) {
                             tabName: 'Board of Trustees',
                             tabContent: (
                               <>
-                                <Panel data={executives} />
+                                <Panel data={bot} />
                               </>
                             ),
                           },
@@ -106,7 +193,7 @@ export default function Executives(props) {
                             tabName: 'Advisory Council',
                             tabContent: (
                               <>
-                                <Panel data={executives} />
+                                <Panel data={ac} />
                               </>
                             ),
                           },
