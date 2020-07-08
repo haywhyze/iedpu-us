2f2630363dc412f231089d4a991d5f464765c830
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-07-08 17:18:14 +0100

    format home section

--- src/app/Sections/Homepage/HomeSection.js
+++ src/app/Sections/Homepage/HomeSection.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React from 'react';
 // @material-ui/core components
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles } from '@material-ui/core/styles';
 
 // core components
-import GridContainer from "components/Grid/GridContainer.js";
-import GridItem from "components/Grid/GridItem.js";
-import styles from "assets/jss/material-kit-react/views/landingPageSections/productStyle.js";
-import { Typography, List, ListItem } from "@material-ui/core";
-import CarouselSection from "./CarouselSection";
+import GridContainer from 'components/Grid/GridContainer.js';
+import GridItem from 'components/Grid/GridItem.js';
+import styles from 'assets/jss/material-kit-react/views/landingPageSections/productStyle.js';
+import { Typography, List, ListItem } from '@material-ui/core';
+import CarouselSection from './CarouselSection';
 
 const useStyles = makeStyles(styles);
 
@@ -21,8 +21,8 @@ export default function HomeSection() {
           <h2 className={classes.title}>About IEDPU </h2>
           <Typography
             variant="body1"
-            style={{ textAlign: "justify" }}
-            style={{ textAlign: "justify" }}
+            style={{ textAlign: 'justify' }}
+            style={{ textAlign: 'justify' }}
             gutterBottom
           >
             Ilorin Emirate Descendants Progressive Union (IEDPU) is a sole
@@ -34,8 +34,8 @@ export default function HomeSection() {
           <br />
           <Typography
             variant="body1"
-            style={{ textAlign: "justify" }}
-            style={{ textAlign: "justify" }}
+            style={{ textAlign: 'justify' }}
+            style={{ textAlign: 'justify' }}
             gutterBottom
           >
             The Union was reputedly set up in 1941 before the disintegration of
@@ -45,16 +45,18 @@ export default function HomeSection() {
           <br />
           <Typography
             variant="body1"
-            style={{ textAlign: "justify" }}
-            style={{ textAlign: "justify" }}
+            style={{ textAlign: 'justify' }}
+            style={{ textAlign: 'justify' }}
             gutterBottom
           >
             The IEDPU, in its efforts to consolidate an active, progressive, and
             democratic body for the development and progress of the Emirate and
-            indigenes, in 2015 incorporated the{" "}
+            indigenes, in 2015 incorporated the
+            {' '}
             <a href="https://www.iedpu.com.ng/docs/iedpu_constitution.pdf">
               IEDPU Constitution
-            </a>{" "}
+            </a>
+            {' '}
             to ensure the realization of its ideals. The provisions of this
             constitution are legally binding on all members, the National
             Executive Council, Zones, and Branches of the Union. Hence, the
