09db0a0f78dfa38c9a614df930fb9010596d98e5
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-07-10 18:19:51 +0100

    implement email notifications

--- src/app/Sections/utils/ConfirmCell.js
+++ src/app/Sections/utils/ConfirmCell.js
@@ -1,16 +1,16 @@
-import React, { useState } from "react";
-import Tooltip from "@material-ui/core/Tooltip";
-import Confirm from "@material-ui/icons/Check";
-import IconButton from "@material-ui/core/IconButton";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import { makeStyles } from "@material-ui/core/styles";
-import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js";
-import { db } from "../../pages/_app";
+import React, { useState } from 'react';
+import Tooltip from '@material-ui/core/Tooltip';
+import Confirm from '@material-ui/icons/Check';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from 'assets/jss/material-dashboard-react/views/dashboardStyle.js';
+import { db } from '../../pages/_app';
 
 const useStyles = makeStyles(styles);
 
@@ -33,23 +33,52 @@ export default function ConfirmCell({
     setOpen(false);
   };
 
-  const verifyUser = (uid) => {
+  const sendWelcomeMail = (uid, name) => {
+    db.collection('mail').add({
+      toUids: [uid],
+      message: {
+        subject: 'Welcome to the IEDPU - USA Family!',
+        text: `
+        Hi, ${name}
+        Your account has been verified and you can now connect with other members of the community.
+
+        You can also keep up with the latest news and events and also settle your membership fees with ease.
+        
+        You can log in now and update your profile so others in the community can get to know about you.
+        
+        Click https://iedpuusa.org/profile to update your profile
+        `,
+        html: `
+        <p>Hi, ${name}</p>
+        <p>Your account has been verified and you can now connect with other members of the community.</p>
+        <p>You can also keep up with the latest news and events and also settle your membership fees with ease.</p>
+        <p>You can log in now and update your profile so others in the community can get to know about you.</p>        
+        Click <a href="https://iedpuusa.org/profile">here</a> to update your profile.
+        <p>Best Regards,</p>
+        <p>IEDPU - USA Team.</p>
+        `,
+      },
+    }).then(() => console.log('Queued email for delivery')).catch((err) => console.log(err));
+  };
+
+  const verifyUser = (uid, name) => {
     // console.log(uid);
-    db.collection("Users")
+    db.collection('Users')
       .doc(uid)
       .update({ verified: true })
-      .then(function () {
-        console.log("Document successfully written!");
-        setSuccessMessage("User successfully verified!");
+      .then(() => {
+        console.log('Document successfully written!');
+        sendWelcomeMail(uid, name);
+        setSuccessMessage('User successfully verified!');
         setSuccessNotification(true);
         setTimeout(() => {
           setSuccessNotification(false);
         }, 3000);
       })
-      .catch(function (error) {
+      .catch((error) => {
         setFailureNotification(true);
         setErrorMessage(error.message);
-        console.error("Error writing document: ", error);
+        console.error('Error writing document: ', error);
       });
     setOpen(false);
   };
@@ -68,7 +97,7 @@ export default function ConfirmCell({
           className={classes.tableActionButton}
         >
           <Confirm
-            className={classes.tableActionButtonIcon + " " + classes.edit}
+            className={`${classes.tableActionButtonIcon} ${classes.edit}`}
           />
         </IconButton>
       </Tooltip>
@@ -78,7 +107,7 @@ export default function ConfirmCell({
         aria-labelledby="alert-dialog-title"
         aria-describedby="alert-dialog-description"
       >
-        <DialogTitle id="alert-dialog-title">{"Verify User"}</DialogTitle>
+        <DialogTitle id="alert-dialog-title">Verify User</DialogTitle>
         <DialogContent>
           <DialogContentText id="alert-dialog-description">
             Are you sure you want to verify this user as a member of IEDPU - USA
@@ -86,7 +115,7 @@ export default function ConfirmCell({
           </DialogContentText>
         </DialogContent>
         <DialogActions>
-          <Button onClick={() => verifyUser(user.id)} color="primary">
+          <Button onClick={() => verifyUser(user.id, user.displayName)} color="primary">
             Yes, please
           </Button>
           <Button onClick={handleClose} color="primary" autoFocus>
