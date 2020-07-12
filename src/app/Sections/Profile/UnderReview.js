4cd61eb365d1582f882976ee49e7626e456a40cf
Yusuf Abdulkarim <haywhyze@gmail.com>
2020-07-12 07:33:51 +0100

    update profile under review

--- src/app/Sections/Profile/UnderReview.js
+++ src/app/Sections/Profile/UnderReview.js
@@ -1,27 +1,56 @@
-import React, { useContext } from "react";
-import Link from "next/link";
-import Button from "components/CustomButtons/Button.js";
-import { AuthContext } from "../../pages/_app";
+import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Link from 'next/link';
+import Button from 'components/CustomButtons/Button.js';
+import { AuthContext } from '../../pages/_app';
+
+const useStyles = makeStyles({
+  root: {
+    maxWidth: 565,
+  },
+  bullet: {
+    display: 'inline-block',
+    margin: '0 2px',
+    transform: 'scale(0.8)',
+  },
+  title: {
+    fontSize: 14,
+  },
+  pos: {
+    marginBottom: 12,
+  },
+});
 
 export default function UnderReview() {
+  const classes = useStyles();
   const { signOut } = useContext(AuthContext);
   return (
     <div className="container-review">
       <div className="overlay-review">
-        <h1>
-          Profile under review. You will be notified by email when you are
-          verified
-        </h1>
-        <Link href="/">
-          <span>
-            <Button color="primary" size="lg">
-              Return to homepage
+        <Card className={classes.root} size="md" variant="outlined">
+          <CardContent>
+            <Typography variant="h4" component="h2">
+              Profile under review. You will be notified by email when you are
+              verified
+            </Typography>
+          </CardContent>
+          <CardActions style={{ display: 'flex', justifyContent: 'center' }}>
+            <Link href="/">
+              <span>
+                <Button color="primary" size="lg">
+                  Return to homepage
+                </Button>
+              </span>
+            </Link>
+            <Button color="danger" onClick={signOut}>
+              Sign out
             </Button>
-          </span>
-        </Link>
-        <Button color="danger" onClick={signOut}>
-          Sign out
-        </Button>
+          </CardActions>
+        </Card>
       </div>
     </div>
   );
